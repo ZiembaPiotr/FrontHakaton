fe9e5586167ef308be54d39928b3486807e4f714
Piotr Ziemba <56030688+ZiembaPiotr@users.noreply.github.com>
2022-04-05 09:54:21 +0200

    Update GoogleMap.js

--- src/components/GoogleMap.js
+++ src/components/GoogleMap.js
@@ -29,8 +29,8 @@ const Map = () => {
   ]);
   return (
     <LoadScript
-      // googleMapsApiKey=process.env.API_KEY
-      googleMapsApiKey="AIzaSyCC7l2an9afwuF1LcCCmhGS0g72zLX0vJ4"
+      googleMapsApiKey=process.env.API_KEY
+      
 
     >
       <GoogleMap
